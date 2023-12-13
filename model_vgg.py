c85c069c574d7678ea7dba2601de9a6a14a986b7
Diogen Babuc <54851778+fortunab@users.noreply.github.com>
2023-12-13 12:12:26 +0200

    Update model_vgg.py

--- model_vgg.py
+++ model_vgg.py
@@ -11,7 +11,7 @@ start = time.time()
 # Define the VGG-16 model
 model = Sequential()
 
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(224, 224, 3)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(227, 227, 3)))
 model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
